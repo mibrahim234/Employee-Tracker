817202c582e6a0709ef00a8abbcc483f842fe1f6
mibrahim234 <mohamm1417@gmail.com>
2022-01-01 01:38:34 -0500

    error server not starting:

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,12 +9,12 @@ INSERT INTO role (title, salary, department_id)
 VALUES
 ("Offensive Coach, 600000, 1"),
 ("Defensive Coach, 500000, 1"),
-("Small Forward, 300000, 2"),
+("Small Forward, 30000, 2"),
 ("Point Guard, 400000, 2"),
 ("Sixth Man, 100000, 3"),
 ("Seventh Man, 50000, 3"),
-("Shoot First, 10000000, 4"),
-("Dribble Finishing, 10000000, 4");
+("Shoot First, 1000000, 4"),
+("Dribble Finishing, 1000000, 4");
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
