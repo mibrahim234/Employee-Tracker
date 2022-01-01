964c3032dc456bcb91a9b9305616899169c3887d
mibrahim234 <mohamm1417@gmail.com>
2021-12-31 22:22:29 -0500

    working on init function

--- server.js
+++ server.js
@@ -13,5 +13,73 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log ();
-    start();
+    init();
   });
+
+ var choices = [
+                 "View All Employees",
+                "View All Departments",
+                "View All Roles",
+                "Add new Employee",
+                "Add new Department",
+                "Add new Role",
+                "Update Employee Role",
+                "Exit",
+ ];
+
+  function init() {
+      getDepartment();
+      getRoles();
+      getEmployee();
+      inquirer
+      .prompt([
+          {
+              type: "list",
+              name: "choice",
+              message: "What would you like to do?",
+              choices: choices,
+          },
+        ])
+            .then(function(answer) {
+              switch (answer.choice) {
+        
+            // Necessary cases
+            case "View All Employees":
+        viewAllEmployees();
+        break;
+
+      case "View All Departments":
+        viewAllDepartments();
+        break;
+
+      case "View All Roles":
+        viewAllRoles();
+        break;
+
+      case "Add new Employee":
+        addEmployee();
+        break;
+
+      case "Add new Department":
+        addDepartment();
+        break;
+
+      case "Add new Role":
+        addRole();
+        break;
+
+      case "Update Employee Role":
+          updateRole();
+          break;
+
+    // Bonus case
+      case "View All Employees by Department":
+        allEmployeesDepartment();
+        break; 
+        
+      case "Exit":
+        connection.end();
+        break;
+      }
+    });
+}
